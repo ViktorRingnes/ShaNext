ba8d458d4106250901cbb3339b4cfa29c93bdf66
ViktorRingnes <viktor_ringnes@icloud.com>
2024-05-31 19:10:57 +0200

    syntax errors and fixed handling

--- ShaNext/Algorithms/Whirpool.cs
+++ ShaNext/Algorithms/Whirpool.cs
@@ -5,105 +5,165 @@ namespace ShaNext.ShaNext
 {
     public class Whirlpool : IHashAlgorithm
     {
-        private static readonly ulong[][] C =
-        [
-            [0x18186018c07830d8, 0x23238c2305af4626, 0xc6c63fc67ef991b8, 0xe8e887e8136fcdfb, 0x87872687a9f5d8d0, 0xb8b8dab8a9626d11, 0x0101040108050209, 0x4f4f214f426e9e0d, 0x3636d836adee6c9b, 0xa6a6a2a6590451ff, 0xd2d26fd2debdb90c, 0xf5f5f3f5fb06f70e, 0x7979f979ef80f296, 0x6f6fa16f5fcede30, 0x91917e91fcef3f6d, 0x52525552aa07a4f8],
-            [0x60609d6027fdc047, 0xbcbccabc89766535, 0x9b9b569baccd2b37, 0x8e8e028e8cbf2354, 0xa3a3b6a371155bd2, 0x0c0c300c603c186c, 0x7b7bf17bff8af684, 0x3535d435b5e16a80, 0x1d1d741de8693af5, 0xe0e0a7e05347ddb3, 0xd7d77bd7f6acb321, 0xc2c22fc25eed999c, 0x2e2eb82e6d965c43, 0x4b4b314b627a9629, 0xfefedffea321e15d, 0x575741578216aed5],
-            [0x15155415a8412abd, 0x7777c1779fb6eee8, 0x3737dc37a5eb6e92, 0xe5e5b3e57b56d79e, 0x9f9f469f8cd92313, 0xf0f0e7f0d317fd23, 0x4a4a354a6a7f9420, 0xdada4fda9e95a944, 0x58587d58fa25b0a2, 0xc9c903c906ca8fcf, 0x2929a429558d527c, 0x0a0a280a5022145a, 0xb1b1feb1e14f7f50, 0xa0a0baa0691a5dc9, 0x6b6bb16b7fdad614, 0x85852e855cabd09e],
-            [0xbdbdcebd8173673c, 0x5d5d695dd234ba8f, 0x1010401080502090, 0xf4f4f7f4f303f507, 0xcbcb0bcb16c08bdd, 0x3e3ef83eedc67cd3, 0x0505140528110a2d, 0x676781671fe6ce78, 0xe4e4b7e47353d597, 0x27279c2725bb4e02, 0x4141194132588273, 0x8b8b168b2c9d0ba7, 0xa7a7a6a7510153f6, 0x7d7de97dcf94fab2, 0x95956e95dcfb3749, 0xd8d847d88e9fad56],
-            [0xfbfbcbfb8b30eb70, 0xeeee9fee2371c1cd, 0x7c7ced7cc791f8bb, 0x6666856617e3cc71, 0xdddd53dda68ea77b, 0x17175c17b84b2eaf, 0x4747014702468e45, 0x9e9e429e84dc211a, 0xcaca0fca1ec589d4, 0x2d2db42d75995a58, 0xbfbfc6bf9179632e, 0x07071c07381b0e3f, 0xadad8ead012347ac, 0x5a5a755aea2fb4b0, 0x838336836cb51bef, 0x3333cc3385ff66b6],
-            [0x636391633ff2c65c, 0x02020802100a0412, 0xaaaa92aa39384993, 0x7171d971afa8e2de, 0xc8c807c80ecf8dc6, 0x19196419c87d32d1, 0x494939497270923b, 0xd9d943d9869aaf5f, 0xf2f2eff2c31df931, 0xe3e3abe34b48dba8, 0x5b5b715be22ab6b9, 0x88881a8834920dbc, 0x9a9a529aa4c8293e, 0x262698262dbe4c0b, 0x3232c8328dfa64bf, 0xb0b0fab0e94a7d59],
-            [0xe9e983e91b6acff2, 0x0f0f3c0f78331e77, 0xd5d573d5e6a6b733, 0x80803a8074ba1df4, 0xbebec2be997c6127, 0xcdcd13cd26de87eb, 0x3434d034bde46889, 0x48483d487a759032, 0xffffdbffab24e354, 0x7a7af57af78ff48d, 0x90907a90f4ea3d64, 0x5f5f615fc23ebe9d, 0x202080201da0403d, 0x6868bd6867d5d00f, 0x1a1a681ad07234ca, 0xaeae82ae192c41b7],
-            [0xb4b4eab4c95e757d, 0x54544d549a19a8ce, 0x93937693ece53b7f, 0x222288220daa442f, 0x64648d6407e9c863, 0xf1f1e3f1db12ff2a, 0x7373d173bfa2e6cc, 0x12124812905a2482, 0x40401d403a5d807a, 0x0808200840281048, 0xc3c32bc356e89b95, 0xecec97ec337bc5df, 0xdbdb4bdb96a0a2ba, 0xa1a1bea1611f5fc0, 0x8d8d0e8d1c830791, 0x3d3df43df5c97ac8],
-        ];
+        private const int HashSize = 64;
+        private const int BlockSize = 64;
 
-        private static readonly ulong[] RC =
-        [
-            0x1823c6e887b8014f, 0x36a6d2f5796f9152, 0x60bc9b8ea30c7b35, 0x1de0d7c22e4bfe57,
-            0x157737e59ff04ada, 0x58c9290ab1a06b85, 0xbd5d10f4cb3e0567, 0xe427418ba77d95d8,
-            0xfbee7c66dd17479e, 0xca2dbf07ad5a8333
-        ];
+        private static readonly ulong[,] C = new ulong[8, 256];
+        private static readonly ulong[] rc = new ulong[10];
 
-        private ulong[] K = new ulong[8];
-        private ulong[] L = new ulong[8];
-        private ulong[] block = new ulong[8];
-        private ulong[] state = new ulong[8];
+        static Whirlpool()
+        {
+            Initialize();
+        }
+
+        public static void Initialize()
+        {
+            ulong[] S = {
+                0x18, 0x23, 0xC6, 0xE8, 0x87, 0xB8, 0x01, 0x4F,
+                0x36, 0xA6, 0xD2, 0xF5, 0x79, 0x6F, 0x91, 0x52,
+                0x60, 0xBC, 0x9B, 0x8E, 0xA3, 0x0C, 0x7B, 0x35,
+                0x1D, 0xE0, 0xD7, 0xC2, 0x2E, 0x4B, 0xFE, 0x57,
+                0x15, 0x77, 0x37, 0xE5, 0x9F, 0xF0, 0x4A, 0xDA,
+                0x58, 0xC9, 0x29, 0x0A, 0xB1, 0xA0, 0x6B, 0x85,
+                0xBD, 0x5D, 0x10, 0xF4, 0xCB, 0x3E, 0x05, 0x67,
+                0xE4, 0x27, 0x41, 0x8B, 0xA7, 0x7D, 0x95, 0xD8,
+                0xFB, 0xEE, 0x7C, 0x66, 0xDD, 0x17, 0x47, 0x9E,
+                0xCA, 0x2D, 0xBF, 0x07, 0xAD, 0x5A, 0x83, 0x33,
+                0x63, 0x02, 0xAA, 0x71, 0xC8, 0x19, 0x49, 0xD9,
+                0xF2, 0xE3, 0x5B, 0x88, 0x9A, 0x26, 0x32, 0xB0,
+                0xE9, 0x0F, 0xD5, 0x80, 0xBE, 0xCD, 0x34, 0x48,
+                0xFF, 0x7A, 0x90, 0x5F, 0x20, 0x68, 0x1A, 0xAE,
+                0xB4, 0x54, 0x93, 0x22, 0x64, 0xF1, 0x73, 0x12,
+                0x40, 0x08, 0xC3, 0xEC, 0xDB, 0xA1, 0x8D, 0x3D,
+                0x97, 0x00, 0xCF, 0x2B, 0x76, 0x82, 0xD6, 0x1B,
+                0xB5, 0xAF, 0x6A, 0x50, 0x45, 0xF3, 0x30, 0xEF,
+                0x3F, 0x55, 0xA2, 0xEA, 0x65, 0xBA, 0x2F, 0xC0,
+                0xDE, 0x1C, 0xFD, 0x4D, 0x92, 0x75, 0x06, 0x8A,
+                0xB2, 0xE6, 0x0E, 0x1F, 0x62, 0xD4, 0xA8, 0x96,
+                0xF9, 0xC5, 0x25, 0x59, 0x84, 0x72, 0x39, 0x4C,
+                0x5E, 0x78, 0x38, 0x8C, 0xD1, 0xA5, 0xE2, 0x61,
+                0xB3, 0x21, 0x9C, 0x1E, 0x43, 0xC7, 0xFC, 0x04,
+                0x51, 0x99, 0x6D, 0x0D, 0xFA, 0xDF, 0x7E, 0x24,
+                0x3B, 0xAB, 0xCE, 0x11, 0x8F, 0x4E, 0xB7, 0xEB,
+                0x3C, 0x81, 0x94, 0xF7, 0xB9, 0x13, 0x2C, 0xD3,
+                0xE7, 0x6E, 0xC4, 0x03, 0x56, 0x44, 0x7F, 0xA9,
+                0x2A, 0xBB, 0xC1, 0x53, 0xDC, 0x0B, 0x9D, 0x6C,
+                0x31, 0x74, 0xF6, 0x46, 0xAC, 0x89, 0x14, 0xE1,
+                0x16, 0x3A, 0x69, 0x09, 0x70, 0xB6, 0xD0, 0xED,
+                0xCC, 0x42, 0x98, 0xA4, 0x28, 0x5C, 0xF8, 0x86
+            };
+
+            for (int x = 0; x < 256; x++)
+            {
+                ulong v1 = S[x];
+                ulong v2 = Rot(v1, 8);
+                ulong v3 = Rot(v1, 16);
+                ulong v4 = Rot(v1, 24);
+                ulong v5 = v1 ^ v3;
+                ulong v6 = v2 ^ v4;
+                ulong v7 = Rot(v5, 32);
+
+                C[0, x] = v1 << 56 | v2 << 48 | v3 << 40 | v4 << 32 | v5 << 24 | v6 << 16 | v7 << 8 | v5;
+                C[1, x] = v2 << 56 | v3 << 48 | v4 << 40 | v5 << 32 | v6 << 24 | v7 << 16 | v5 << 8 | v1;
+                C[2, x] = v3 << 56 | v4 << 48 | v5 << 40 | v6 << 32 | v7 << 24 | v5 << 16 | v1 << 8 | v2;
+                C[3, x] = v4 << 56 | v5 << 48 | v6 << 40 | v7 << 32 | v5 << 24 | v1 << 16 | v2 << 8 | v3;
+                C[4, x] = v5 << 56 | v6 << 48 | v7 << 40 | v5 << 32 | v1 << 24 | v2 << 16 | v3 << 8 | v4;
+                C[5, x] = v6 << 56 | v7 << 48 | v5 << 40 | v1 << 32 | v2 << 24 | v3 << 16 | v4 << 8 | v5;
+                C[6, x] = v7 << 56 | v5 << 48 | v1 << 40 | v2 << 32 | v3 << 24 | v4 << 16 | v5 << 8 | v6;
+                C[7, x] = v5 << 56 | v1 << 48 | v2 << 40 | v3 << 32 | v4 << 24 | v5 << 16 | v6 << 8 | v7;
+            }
+
+            rc[0] = 0x1823C6E887B8014F;
+            for (int r = 1; r < 10; r++)
+            {
+                rc[r] = (ulong)((rc[r - 1] >> 8) ^ C[0, (byte)rc[r - 1]]);
+            }
+        }
+
+        public static ulong Rot(ulong x, int s)
+        {
+            return (x << s) | (x >> (64 - s));
+        }
+
+        public static byte[] PadMessage(byte[] message)
+        {
+            ulong bitLength = (ulong)message.Length * 8;
+            int padLength = (message.Length % BlockSize < 32) ? (32 - message.Length % BlockSize) : (96 - message.Length % BlockSize);
+            byte[] paddedMessage = new byte[message.Length + padLength + 32];
+            Buffer.BlockCopy(message, 0, paddedMessage, 0, message.Length);
+            paddedMessage[message.Length] = 0x80;
+            for (int i = paddedMessage.Length - 32; i < paddedMessage.Length - 16; i++)
+            {
+                paddedMessage[i] = 0x00;
+            }
+            Buffer.BlockCopy(BitConverter.GetBytes(ReverseBytes(bitLength)), 0, paddedMessage, paddedMessage.Length - 16, 8);
+            return paddedMessage;
+        }
+
+        public static ulong ReverseBytes(ulong x)
+        {
+            return ((x & 0x00000000000000FFUL) << 56) |
+                   ((x & 0x000000000000FF00UL) << 40) |
+                   ((x & 0x0000000000FF0000UL) << 24) |
+                   ((x & 0x00000000FF000000UL) << 8)  |
+                   ((x & 0x000000FF00000000UL) >> 8)  |
+                   ((x & 0x0000FF0000000000UL) >> 24) |
+                   ((x & 0x00FF000000000000UL) >> 40) |
+                   ((x & 0xFF00000000000000UL) >> 56);
+        }
 
         public byte[] ComputeHash(byte[] input)
         {
-            int paddedLength = input.Length + 1 + 31;
-            paddedLength -= paddedLength % 32;
-            byte[] paddedInput = new byte[paddedLength];
-            Buffer.BlockCopy(input, 0, paddedInput, 0, input.Length);
-            paddedInput[input.Length] = 0x80;
-            ulong bitLength = (ulong)input.Length * 8;
-            for (int i = 0; i < 8; i++)
-            {
-                paddedInput[paddedInput.Length - 1 - i] = (byte)(bitLength >> (8 * i));
-            }
+            byte[] paddedMessage = PadMessage(input);
+            ulong[] H = new ulong[8];
 
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < paddedMessage.Length / BlockSize; i++)
             {
-                state[i] = 0;
-            }
+                ulong[] K = new ulong[8];
+                ulong[] block = new ulong[8];
+                ulong[] state = new ulong[8];
 
-            for (int i = 0; i < paddedInput.Length; i += 32)
-            {
                 for (int j = 0; j < 8; j++)
                 {
-                    block[j] = BitConverter.ToUInt64(paddedInput, i + j * 8);
+                    state[j] = H[j] ^ BitConverter.ToUInt64(paddedMessage, (i * BlockSize) + (j * 8));
+                    K[j] = state[j];
                 }
-                ProcessBlock();
+
+                for (int r = 0; r < 10; r++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        block[j] = 0;
+                        block[j] ^= C[0, (byte)(K[j] >> 56)];
+                        block[j] ^= C[1, (byte)(K[(j + 7) % 8] >> 48)];
+                        block[j] ^= C[2, (byte)(K[(j + 6) % 8] >> 40)];
+                        block[j] ^= C[3, (byte)(K[(j + 5) % 8] >> 32)];
+                        block[j] ^= C[4, (byte)(K[(j + 4) % 8] >> 24)];
+                        block[j] ^= C[5, (byte)(K[(j + 3) % 8] >> 16)];
+                        block[j] ^= C[6, (byte)(K[(j + 2) % 8] >> 8)];
+                        block[j] ^= C[7, (byte)(K[(j + 1) % 8])];
+                    }
+
+                    for (int j = 0; j < 8; j++)
+                    {
+                        K[j] = block[j];
+                    }
+
+                    K[0] ^= rc[r];
+                }
+
+                for (int j = 0; j < 8; j++)
+                {
+                    H[j] ^= K[j] ^ state[j];
+                }
             }
 
-            byte[] hash = new byte[64];
+            byte[] hash = new byte[HashSize];
             for (int i = 0; i < 8; i++)
             {
-                byte[] temp = BitConverter.GetBytes(state[i]);
-                Array.Reverse(temp);
-                Buffer.BlockCopy(temp, 0, hash, i * 8, 8);
+                Buffer.BlockCopy(BitConverter.GetBytes(ReverseBytes(H[i])), 0, hash, i * 8, 8);
             }
             return hash;
-        }
-
-        private void ProcessBlock()
-        {
-            for (int i = 0; i < 8; i++)
-            {
-                state[i] ^= block[i];
-                K[i] = state[i];
-            }
-
-            for (int r = 0; r < 10; r++)
-            {
-                for (int i = 0; i < 8; i++)
-                {
-                    L[i] = 0;
-                    for (int j = 0; j < 8; j++)
-                    {
-                        L[i] ^= C[j][(K[(i - j) & 7] >> ((7 - j) * 8)) & 0xFF];
-                    }
-                }
-
-                Array.Copy(L, K, 8);
-                K[0] ^= RC[r];
-
-                for (int i = 0; i < 8; i++)
-                {
-                    L[i] = 0;
-                    for (int j = 0; j < 8; j++)
-                    {
-                        L[i] ^= C[j][(state[(i - j) & 7] >> ((7 - j) * 8)) & 0xFF];
-                    }
-                }
-
-                Array.Copy(L, state, 8);
-            }
-
-            for (int i = 0; i < 8; i++)
-            {
-                state[i] ^= block[i];
-            }
         }
 
         public byte[] ComputeHash(string input)
